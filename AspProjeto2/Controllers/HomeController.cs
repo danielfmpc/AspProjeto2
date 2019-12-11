7b899f9b8be5c088fc666166c9b9fa92150f1f23
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2019-12-11 17:11:56 -0300

    Criando Departments

--- AspProjeto2/Controllers/HomeController.cs
+++ AspProjeto2/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using AspProjeto2.Models;
+using AspProjeto2.Models.ViewModels;
 
 namespace AspProjeto2.Controllers
 {
--- AspProjeto2/Models/ViewModels/ErrorViewModel.cs
+++ AspProjeto2/Models/ViewModels/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace AspProjeto2.Models
+namespace AspProjeto2.Models.ViewModels
 {
     public class ErrorViewModel
     {
