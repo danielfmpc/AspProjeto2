13165609fe04694941779251395c31546315ce96
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2019-12-12 08:45:46 -0300

    CRUD Scaffolding

---
+++ AspProjeto2/Data/AspProjeto2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AspProjeto2.Models;
+
+namespace AspProjeto2.Data
+{
+    public class AspProjeto2Context : DbContext
+    {
+        public AspProjeto2Context (DbContextOptions<AspProjeto2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AspProjeto2.Models.Department> Department { get; set; }
+    }
+}
